58d87824703120c64d22cafb9bdeaa087c0ee42f
Vishweshwarayya Kj <k.vishu@outlook.com>
2023-11-23 23:18:16 +0530

    Portfolio: Code updates, SVG enhancements

---
+++ assets/CoverLetter-afa770ee.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,M as r}from"./index-2ce51840.js";const _={__name:"CoverLetter",setup(a){return(c,o)=>(e(),t(r,{class:"cover_letter"}))}};export{_ as default};
--- assets/CoverLetter-c28da625.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,M as r}from"./index-d1718a5e.js";const _={__name:"CoverLetter",setup(a){return(c,o)=>(e(),t(r,{class:"cover_letter"}))}};export{_ as default};
