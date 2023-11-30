0599ba65af3c5a66b2517bc2ef74f46781486a4d
Vishweshwarayya Kj <k.vishu@outlook.com>
2023-11-30 21:18:11 +0530

    Portfolio: Changed back to static files

--- src/utils/ErrorMessages.js
+++
@@ -1,7 +0,0 @@
-const ErrorMessages = {
-  VALID_OBJECT: 'Please provide valid object!',
-  VALID_ARRAY: 'Please provide valid data array!',
-  NO_DUPLICATES: `Please don't duplicate`
-}
-
-export default ErrorMessages
