9cfdcf2ebc3c4841e454a3b20f478a73458754af
Vishweshwarayya Kj <k.vishu@outlook.com>
2023-11-30 23:29:14 +0530

    Portfolio: Changed back to static files

--- assets/AboutDev-a6c4d419.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as _,a as o,D as s}from"./index-048b9006.js";const a={class:"about_dev"},c="Vishweshwarayya Kj",r={__name:"AboutDev",setup(n){return(p,u)=>(t(),_("div",a,[o(s,{titleText:c})]))}},v=e(r,[["__scopeId","data-v-8f900eb2"]]);export{v as default};
