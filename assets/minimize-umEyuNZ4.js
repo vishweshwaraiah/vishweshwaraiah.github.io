4ce5959f0ed358fac854be345eb70fb12fe060c7
Vishweshwarayya Kj <k.vishu@outlook.com>
2024-01-06 23:44:42 +0530

    Portfolio: Responsive changes and Home page added!

---
+++ assets/minimize-umEyuNZ4.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as o}from"./index-R7Dn8XFA.js";const s={xmlns:"http://www.w3.org/2000/svg","xml:space":"preserve",viewBox:"0 0 83 83"},a=o("path",{d:"M81 36.166H2a2 2 0 0 0-2 2v6.668a2 2 0 0 0 2 2h79a2 2 0 0 0 2-2v-6.668a2 2 0 0 0-2-2"},null,-1),c=[a];function n(r,i){return e(),t("svg",s,[...c])}const l={render:n};export{l as default,n as render};
