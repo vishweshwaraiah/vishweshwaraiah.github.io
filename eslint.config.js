6646a12c785dc8cf1ad844201c9a1288490fe5c9
Vishweshwarayya Kj <k.vishu@outlook.com>
2024-10-07 19:23:40 +0530

    Keep calm and commit, added husky check

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest'
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error'
+    }
+  }
+]
