64854e8a3fe8bc990a14ff6aec63cda4471cc39a
Vishweshwarayya Kj <k.vishu@outlook.com>
2024-10-08 00:08:02 +0530

    Portfolio: Updated aliases to fix build error

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  publicDir: 'public',
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: [
@@ -26,17 +27,10 @@ export default defineConfig({
         replacement: fileURLToPath(new URL('./src/components', import.meta.url))
       }
     ]
-    // {
-    //   '@': fileURLToPath(new URL('./', import.meta.url)),
-    //   '@src': fileURLToPath(new URL('./src', import.meta.url)),
-    //   '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-    //   '@components': fileURLToPath(new URL('./src/components', import.meta.url))
-    // }
   },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern',
         additionalData: `
             @import "@assets/styles/_fonts.scss";
             @import "@assets/styles/_variables.scss";
